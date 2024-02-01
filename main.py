553b4eef2460a14bf9c14748a6fd22b806025de2
Micah Maphet <micahmaphet@gmail.com>
2024-02-01 18:26:27 -0500

    created data flow, model, and training sequence

--- main.py
+++ main.py
@@ -1,19 +1,32 @@
 import keras
+from keras import layers
+from keras import models
+from keras.utils import image_dataset_from_directory
 
-train_ds = keras.utils.image_dataset_from_directory(
-    directory='dogs-vs-cats/',
-    labels='inferred',
-    label_mode='categorical',
-    batch_size=32,
-    image_size=(256, 256))
-validation_ds = keras.utils.image_dataset_from_directory(
-    directory='dogs-vs-cats/',
-    labels='inferred',
-    label_mode='categorical',
-    batch_size=32,
-    image_size=(256, 256))
 
-model = keras.applications.Xception(
-    weights=None, input_shape=(256, 256, 3), classes=10)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
-model.fit(train_ds, epochs=10, validation_data=validation_ds)
+train_ds = image_dataset_from_directory(
+    directory='train/',
+    labels='inferred',
+    label_mode='binary',
+    batch_size=20,
+    image_size=(150, 150)
+)
+
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1, activation='sigmoid')
+])
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_ds, epochs=10)
