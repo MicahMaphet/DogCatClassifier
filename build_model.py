23d7b3d9bd125af999079f443d722ec0f5659d38
Micah Maphet <micahmaphet@gmail.com>
2024-02-02 17:45:17 -0500

    added more layers to the network and added predict implementation

--- build_model.py
+++ build_model.py
@@ -2,6 +2,8 @@ import keras
 from keras import layers
 from keras import models
 from keras.utils import image_dataset_from_directory
+import tensorflow as tf
+import matplotlib.pyplot as plt
 
 
 train_ds = image_dataset_from_directory(
@@ -13,6 +15,14 @@ train_ds = image_dataset_from_directory(
 )
 
 
+val_ds = image_dataset_from_directory(
+    directory='test/',
+    labels='inferred',
+    label_mode='binary',
+    batch_size=20,
+    image_size=(150, 150)
+)
+
 model = models.Sequential([
     layers.Conv2D(32, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
@@ -22,6 +32,8 @@ model = models.Sequential([
     layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
     layers.Dense(64, activation='relu'),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(64, activation='relu'),
     layers.Dense(1, activation='sigmoid')
 ])
 
@@ -29,6 +41,17 @@ model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 
-history = model.fit(train_ds, epochs=10)
+history = model.fit(train_ds, epochs=5, validation_data=val_ds)
+
+print("Plotting model:")
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+plt.show()
 
 model.save('model.keras')
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import keras
 from keras import models
+import numpy as np
+
+
+images = np.array([
+    keras.utils.img_to_array(keras.utils.load_img('test/cat/cat.0.jpg', target_size=(150, 150))),
+    keras.utils.img_to_array(keras.utils.load_img('test/cat/cat.1.jpg', target_size=(150, 150))),
+    keras.utils.img_to_array(keras.utils.load_img('test/dog/dog.0.jpg', target_size=(150, 150))),
+    keras.utils.img_to_array(keras.utils.load_img('test/dog/dog.1.jpg', target_size=(150, 150)))
+])
 
 model = models.load_model('model.keras')
+predictions = model.predict(images)
+
+print('Cat, Cat, Dog, Dog')
+print(predictions)
