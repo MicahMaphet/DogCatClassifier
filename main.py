605e41a9fbde19bc9dafe83da25ec14744d0389e
Micah Maphet <micahmaphet@gmail.com>
2024-02-01 19:28:56 -0500

    refactored main.py to build.py and replaced it with model loading

--- main.py
+++ main.py
@@ -1,32 +1,3 @@
-import keras
-from keras import layers
 from keras import models
-from keras.utils import image_dataset_from_directory
 
-
-train_ds = image_dataset_from_directory(
-    directory='train/',
-    labels='inferred',
-    label_mode='binary',
-    batch_size=20,
-    image_size=(150, 150)
-)
-
-
-model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Flatten(),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(1, activation='sigmoid')
-])
-
-model.compile(optimizer='adam',
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
-
-history = model.fit(train_ds, epochs=10)
+model = models.load_model('model.keras')
