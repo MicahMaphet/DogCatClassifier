37830360a25123effd920626bb194abef3093686
Micah Maphet <micahmaphet@gmail.com>
2024-02-06 12:44:02 -0500

    added image creation of activation image

--- main.py
+++ main.py
@@ -9,6 +9,13 @@ import matplotlib.pyplot as plt
 
 file_path = ''
 model = models.load_model('model.keras')
+
+layer_names = []
+for layer in model.layers:
+    layer_names.append(layer.name)
+
+print(layer_names)
+
 def open_image():
     global file_path
     file_path = filedialog.askopenfilename()
@@ -32,18 +39,19 @@ def predict():
 
 def show_activation():
     global file_path
-    print('File path: ' + file_path)
     image = np.array([ img_to_array(load_img(file_path, target_size=(150, 150))) ])
     layer_outputs = [layer.output for layer in model.layers]
     activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
     activations = activation_model.predict(image)
     layer_activation = activations[int(layer_index.get())]
-    print(layer_activation)
-    print(layer_activation.shape)
-    plt.matshow(layer_activation[0, :, :, int(channel_index.get())], cmap='viridis')
-    plt.show()
 
-    
+    imageData = layer_activation[0, :, :, int(channel_index.get())]
+    imageData = (imageData - imageData.min()) / (imageData.max() - imageData.min()) * 256
+    picture = Image.fromarray(imageData)
+    picture.convert('RGB').save('activation.png')
+    # normalize array
+    photo = ImageTk.PhotoImage(image=Image.fromarray(imageData))
+    activation_image.create_image(20, 20, anchor="nw", image=photo)
 
 # Create the main window
 root = tk.Tk()
@@ -54,9 +62,6 @@ root.title('Dog and Cat Predictor')
 input_image = tk.Label(root)
 input_image.pack()
 
-activation_image = tk.Label(root)
-activation_image.pack()
-
 # Create a button to open the image
 open_button = tk.Button(root, text='Open Image', command=open_image)
 open_button.pack(pady=10)
@@ -65,20 +70,26 @@ open_button.pack(pady=10)
 predict_button = tk.Button(root, text='Predict', command=predict)
 predict_button.pack(pady=10)
 
-layer_activation_button = tk.Button(root, text='Layer Activation', command=show_activation)
-layer_activation_button.pack(pady=10)
-
-tk.Scale(root, from_=0, to=len(model.layers), orient='horizontal').pack()
-layer_index = tk.Entry(root, width=20)
-layer_index.pack()
-
-tk.Scale(root, from_=0, to=128).pack()
-channel_index = tk.Entry(root, width=20)
-channel_index.pack()
-
 # Create a label to display the prediction
 prediction_label = tk.Label(root, text="")
 prediction_label.pack()
+
+# Frame for activation analysis
+activation_frame = tk.Frame(root)
+activation_frame.pack()
+
+activation_image = tk.Canvas(activation_frame, width=150, height=150)
+activation_image.pack()
+
+layer_activation_button = tk.Button(activation_frame, text='Layer Activation', command=show_activation)
+layer_activation_button.pack(pady=10)
+
+layer_index = tk.Scale(activation_frame, from_=0, to=len(model.layers), orient='horizontal', command=show_activation)
+layer_index.pack()
+
+channel_index = tk.Scale(activation_frame, from_=0, to=128, orient='horizontal', command=show_activation)
+channel_index.pack()
+
 
 
 # Run the application
